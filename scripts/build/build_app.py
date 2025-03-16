6b69b72dced9f5561c8378d60f933a15a3684878
Siddartha Kodaboina <k.siddartha49@gmail.com>
2025-03-15 19:42:16 -0700

    Fix: get the workspace path correct

--- scripts/build/build_app.py
+++ scripts/build/build_app.py
@@ -6,14 +6,14 @@ import subprocess
 import sys
 from pathlib import Path
 
-def build_application(app_name: str):
+def build_application(app_name: str, workspace_root: str):
     """
     Builds the application using Docker
     """
     try:
         # Get the application directory path (two levels up from current script and into app directory)
-        workspace_root = Path(os.getcwd()).parent.parent
-        app_path = workspace_root.parent / app_name
+        # workspace_root = Path(os.getcwd()).parent.parent
+        app_path = workspace_root / app_name / app_name
         
         print(f"Building application in: {app_path}")
         
@@ -39,11 +39,12 @@ def build_application(app_name: str):
 
 def main():
     parser = argparse.ArgumentParser(description="Build application Docker image")
+    parser.add_argument("--workspace-root", required=True, help="Workspace root Path")
     parser.add_argument("--app-name", required=True, help="Application name")
     
     args = parser.parse_args()
     
-    return build_application(args.app_name)
+    return build_application(args.app_name, args.workspace_root)
 
 if __name__ == "__main__":
     sys.exit(main())
